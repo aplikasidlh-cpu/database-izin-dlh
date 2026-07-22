807481f4f8c843257835f93855dbd9588b35ee07
aplikasidlh-cpu <aplikasidlh@gmail.com>
2026-07-22 12:11:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Konfigurasi koneksi ke database Supabase kamu
+const SUPABASE_URL = "https://kxnesqhygorxcumgxpvb.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ciymmZvR4HjPFK7Qkara3g_lKh2uMlH";
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
